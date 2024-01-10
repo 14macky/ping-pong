39fe248212ca1fc257cbebb6b6b4ef5ecf80c051
14macky <152778980+14macky@users.noreply.github.com>
2024-01-10 16:44:38 +0700

    mission_complete

--- ping-pong.py
+++ ping-pong.py
@@ -1,5 +1,8 @@
 from pygame import *
 import time as t
+from random import *
+font.init()
+default_font = font.Font(None, 80)
 
 width = 600
 height = 500
@@ -72,16 +75,51 @@ class Ball(GameSprite):
                 self.on_bounce_cooldown = False
 
 ball = Ball("tensis_ball.png", 200, 200,50, 50, 4)
+p1_score = 0
+p2_score = 0
+
+paused = False
 while game_over == False:
     for e in event.get():
         if e.type == QUIT:
             game_over = True
 
-    window.fill(background_color)
-    player_group.draw(window)
-    ball.draw()
-    player_1.update_1()
-    player_2.update_2()
-    ball.update()
+    if paused == False:
+        window.fill(background_color)
+        player_group.draw(window)
+        ball.draw()
+        player_1.update_1()
+        player_2.update_2()
+        ball.update()
+        if ball.rect.x <= 0:
+            lose_text = default_font.render("p1 lost hehe", True, (255, 255, 255))
+            window.blit(lose_text, (150, 200))
+            p2_score += 1
+            paused = True
+            paused_start = t.time()
+        if ball.rect.right >= width:
+            lose_text = default_font.render("p2 lost hehe", True, (255, 255, 255))
+            window.blit(lose_text, (150, 200))
+            p1_score += 1
+            paused = True
+            paused_start = t.time()
+
+        p1_score_text = default_font.render(str(p1_score), True, (255, 255, 255))
+        dash_text = default_font.render(" - ", True, (255, 255, 255))
+        p2_score_text = default_font.render(str(p2_score), True, (255, 255, 255))
+
+        window.blit(p1_score_text, (230, 0))
+        window.blit(dash_text, (275, 0))
+        window.blit(p2_score_text, (340, 0))
+    else:
+        if t.time() - paused_start >= 1:
+            player_1 = Player("racket.png", 0, 200, 50, 150, 4)
+            player_2 = Player("racket.png", 550, 200, 50, 150, 4)
+            player_group = sprite.Group()
+            player_group.add(player_1)
+            player_group.add(player_2)
+            ball = Ball("tensis_ball.png", 200, 200, 50, 50, 4)
+            paused = False
+
     display.update()
     clock.tick(60)
